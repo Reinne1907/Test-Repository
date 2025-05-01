9cf704c7f97c11853e5e868da95800eafcbc77d2
Reinne1907 <itzydroplet@gmail.com>
2025-05-01 17:45:25 +0800

    Update cmdcall.py
    Inserted command call for import file commands.py

--- cmdcall.py
+++ cmdcall.py
@@ -1,1 +1,18 @@
 # cmdcall.py
+
+from commands import commandlist 
+
+def main():
+    print("--- Welcome to Reinne's Testing Place!! ---")
+    while True:
+        pRequest = input("Enter command: ").lower()
+        if pRequest in commandlist:
+            commandlist[pRequest]()
+        elif pRequest == "exit":
+            print("Goodbye")
+            break
+        else:
+            print(f"'{pRequest}' is an invalid command")
+
+if __name__ == "__main__":
+    main()
